90afbf30e2178a057510d29ff0acbc90ff54ab75
JamesFThomas <james.fthomas@yahoo.com>
2022-09-11 11:11:54 -0500

    label & input style change for invalid inputs

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,25 +3,45 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = props => {
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  //create state variable to track valid text entries
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
+  const goalInputChangeHandler = (event) => {
+    // if input text is valid show regular styling
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    } else {
+      setEnteredValue(event.target.value);
+    }
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    // evaluate if text entry is blank spaces
+    if (enteredValue.trim().length === 0) {
+      // update state to indicate blank entry
+      setIsValid(false);
+    } else {
+      props.onAddGoal(enteredValue);
+    }
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+      <div className='form-control'>
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input
+          type='text'
+          style={{
+            borderColor: !isValid ? 'red' : '#ccc',
+            background: !isValid ? 'salmon' : 'transparent',
+          }}
+          onChange={goalInputChangeHandler}
+        />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type='submit'>Add Goal</Button>
     </form>
   );
 };
